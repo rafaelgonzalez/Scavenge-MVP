57c390ab39009b957de932b400646580fb8823cf
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-06-28 21:07:44 +1000

    Add rigidbody and script to player

---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+	public float speed = 5f;
+
+	Vector3 movement;
+	Transform playerTransform;
+	Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = GetComponent<Transform> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontalAxisOffset = Input.GetAxis ("Horizontal");
+		float verticalAxisOffset = Input.GetAxis ("Vertical");
+
+		Move (horizontalAxisOffset, verticalAxisOffset);
+	}
+
+	void Move(float horizontalAxisOffset, float verticalAxisOffset) {
+		movement.Set (horizontalAxisOffset, 0f, verticalAxisOffset);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (playerTransform.position + movement);
+	}
+}
