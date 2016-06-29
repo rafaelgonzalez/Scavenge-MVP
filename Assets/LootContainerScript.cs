921be0ee211275db64f56cd7d63ef7b7d38ab0e2
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-06-29 20:51:57 +1000

    Add loot container with minimal script

---
+++ Assets/LootContainerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Renderer))]
+public class LootContainerScript : MonoBehaviour {
+
+	public bool hasLoot = false;
+
+	private Color lootColor = Color.yellow;
+	private Color noLootColor = Color.gray;
+
+	// Use this for initialization
+	void Start () {
+		if (hasLoot == true) {
+			GetComponent<Renderer>().material.color = Color.yellow;
+		}
+		else {
+			GetComponent<Renderer>().material.color = Color.grey;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
