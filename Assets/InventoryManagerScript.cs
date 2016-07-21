dfd3e992bb11c49f70a9d424fcec27f51322847d
Rafael Gonzalez <rafaelgonzalez@users.noreply.github.com>
2016-07-21 20:41:54 +1000

    Update scripts to render picked up objects in UI

--- Assets/InventoryManagerScript.cs
+++ Assets/InventoryManagerScript.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class InventoryManagerScript : MonoBehaviour {
 
-	public int items = 0;
+	int itemCount = 0;
+	public int totalItems = 0;
+
+	public LootPanelScript lootPanelScript;
 
 	// Use this for initialization
 	void Start () {
-
+		lootPanelScript.setLootedQuantity(itemCount);
+		lootPanelScript.setMaxLootableQuantity(totalItems);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void incrementItemCount() {
+		itemCount += 1;
+
+		lootPanelScript.setLootedQuantity (itemCount);
 	}
 }
